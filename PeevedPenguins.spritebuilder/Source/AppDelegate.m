5462d0911f5f50e27f1aa7beb23d746f6659ec5d
Brandon Gannicott <brandongannicott@Sohils-MacBook-Pro.local>
2015-01-27 22:33:28 -0800

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/AppDelegate.m
+++ PeevedPenguins.spritebuilder/Source/AppDelegate.m
@@ -54,9 +54,6 @@
     
     [self setupCocos2dWithOptions:cocos2dSetup];
     
-    //Test that our custom classes work
-    [CCBReader load:@"Penguin"];
-    [CCBReader load:@"Seal"];
     
     return YES;
 }
