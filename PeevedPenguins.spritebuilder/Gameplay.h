c36118dd0b8411f94a6928aebe1d2c9556183832
Brandon Gannicott <brandongannicott@Sohils-MacBook-Pro.local>
2015-01-28 00:23:49 -0800

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Brandon Gannicott on 1/28/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end
